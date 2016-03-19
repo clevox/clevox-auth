fa04a989046b74b644a11bccfafba8df82f050ef
destromas1 <shahjada.talukdar@gmail.com>
2016-03-19 22:47:06 +0600

    initiated ping-pong

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-var server = require('http').createServer()
-  , port = 5001;
+var restify = require('restify'),
+    port = 5001;
+
+var server = restify.createServer();
+
+server.get('/ping', function (req, res, next) {
+  res.send("pong");
+});
 
 
 server.listen(port, function () { console.log('Listening on ' + server.address().port) });
