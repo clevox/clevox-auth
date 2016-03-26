bd7beb276c6b4464cc5002ea3c1917921465b01c
destromas1 <shahjada.talukdar@gmail.com>
2016-03-27 03:11:13 +0600

    Reg Handler Added

--- routes.js
+++ routes.js
@@ -3,6 +3,14 @@ module.exports = function(app) {
     app.get('/ping', function(req, res, next) {
         res.send("pong");
     });
+    
+    var registrationHandler = require('../app/handlers/registration-handler');
+	// var allowedFeaturesHandler = require('../app/handlers/allowed-features-handler');
+	
+	// app.get('/api/IsFeatureAllowed/:id', featureAllowedHandler.IsFeatureAllowed);
+	
+	app.get('/api/', registrationHandler);
+    
 
 };
 
