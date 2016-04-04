867b0ac48fd18817fd6e1ecb85abc7ee790455f0
Shahjada <shahjada.talukdar@gmail.com>
2016-04-04 14:51:41 +0600

    Middleware bodyParser

--- app/handlers/registration-handler.js
+++ app/handlers/registration-handler.js
@@ -2,7 +2,7 @@ var userModel = require('../models/user');
 
 var registerUser = function(req, res) {
 
-  console.log("registerUser");
+  console.log("registerUser", req.body);
   
   res.send({ success: true });
 
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var restify = require('restify'),
 
 var server = restify.createServer();
 
+// Middlewares
+server.use(restify.bodyParser());
+
 var routes = require('./routes');
 
 routes(server);
-
 
 server.listen(port, function () { console.log('Listening on ' + server.address().port) });
 
