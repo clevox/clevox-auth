4c8e87d4f2533ebdd34b35f233c5c5b22384bfed
destromas1 <shahjada.talukdar@gmail.com>
2016-05-14 23:27:55 +0600

    user Registration Model saving

--- app/models/user.js
+++ app/models/user.js
@@ -1,21 +1,27 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var saveUser = function(user) {
+var insertDocument = function (db, user, callback) {
+  
+};
 
-	
-	MongoClient.connect('mongodb://localhost:27017/AuthDB', function(err , db) {
+var saveUser = function (user) {
+  MongoClient.connect('mongodb://localhost:27017/AuthDB', function (err, db) {
 
-		console.log("user passed", user);
+    console.log("user passed", user);
 
-		if(err) {
-			console.log(err);
-			return;
-		}
-	});
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-//}
+    insertDocument(db, { userId: user.userId, email: user.email, password : user.password, createdDate: new Date() }, function () {
+      db.close();
+    });
+
+  });
+
 };
 
 module.exports = {
-	saveUser : saveUser
+  saveUser: saveUser
 };
