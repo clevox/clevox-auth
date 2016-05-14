a3e2e72324cc8401c6beba8ba7eeda9698505c14
destromas1 <shahjada.talukdar@gmail.com>
2016-05-15 01:12:37 +0600

    user saved

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,15 @@
 var MongoClient = require('mongodb').MongoClient;
 
 var insertDocument = function (db, user, callback) {
-  
+  console.log("user------------------------------>",user);
+  db.collection('user').insertOne(user, function (err, result) {
+    
+    console.log("Inserted User.");
+    callback(result);
+  });
 };
 
-var saveUser = function (user) {
+var saveUser = function (user , callback) {
   MongoClient.connect('mongodb://localhost:27017/AuthDB', function (err, db) {
 
     console.log("user passed", user);
@@ -13,9 +18,10 @@ var saveUser = function (user) {
       console.log(err);
       return;
     }
-
+    
     insertDocument(db, { userId: user.userId, email: user.email, password : user.password, createdDate: new Date() }, function () {
       db.close();
+      callback();
     });
 
   });
