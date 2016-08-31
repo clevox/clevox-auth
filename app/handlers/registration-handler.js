bfd379df1591e833daf1d6965583afa835dafa3a
Shahjada <shahjada.talukdar@gmail.com>
2016-08-31 23:58:49 +0600

    encrypt password

--- app/handlers/registration-handler.js
+++ app/handlers/registration-handler.js
@@ -5,21 +5,19 @@ var crypto = require('crypto'),
     secret = 'AqwerersdfaAqwWqwTtd6F$#%^&*546363463456093498234098324092384928340234qdhjahBHDWSAWEJbsbcdsfkjher2%345sdgsdg';
 
 function encrypt(text){
-  var cipher = crypto.createCipher(algorithm,secret)
-  var crypted = cipher.update(text,'utf8','hex')
+  var cipher = crypto.createCipher(algorithm, secret);
+  var crypted = cipher.update(text, 'utf8', 'hex');
   crypted += cipher.final('hex');
   return crypted;
 }
 
-var registerUser = function(req, res) {
-
+var registerUser = function (req, res) {
+  req.body.password = encrypt(req.body.password);
   user.saveUser(req.body, function () {
     console.log("registerUser", req.body);
     res.send({ success: true });
   });
   
-  
-
 };
 
 
