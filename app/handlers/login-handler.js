6b2f7d68211aeb175e0919ca9c7696c38c00faad
Shahjada <shahjada.talukdar@gmail.com>
2016-09-01 00:05:30 +0600

    encrypt login password

--- app/handlers/login-handler.js
+++ app/handlers/login-handler.js
@@ -1,11 +1,24 @@
 var user = require('../models/user');
 var tokenHelper = require('./../helpers/token-helper');
 
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    secret = 'AqwerersdfaAqwWqwTtd6F$#%^&*546363463456093498234098324092384928340234qdhjahBHDWSAWEJbsbcdsfkjher2%345sdgsdg';
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm, secret);
+  var crypted = cipher.update(text, 'utf8', 'hex');
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
 var login = function(req, res) {
 
   var userEmail = req.body.email;
   var password = req.body.password;
   console.log("Login Request came", userEmail, password);
+
+  password = encrypt(password);
   
   user.checkUserForLogin(userEmail, password, function (err, user) {
     
